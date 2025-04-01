01b1d8331348e9ded4b61b3ac1fb61c46501ce63
Clark <galaxy.games199@gmail.com>
2025-03-31 23:03:23 -0300

    contador da data do evento

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -532,7 +532,27 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"fFaKF":[function(require,module,exports) {
-console.log("hello world");
+AOS.init();
+const dataDoEvento = new Date("Jul 30, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundoAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / 1000);
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h
+    ${minutosAteOEvento}m ${segundoAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento finalizado";
+    }
+}, 1000);
 
 },{}]},["j98HG","fFaKF"], "fFaKF", "parcelRequiredb58")
 
